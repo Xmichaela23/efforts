735f72e8d07b5a819220a1a42d0fb7298d767920
Xmichael23 <217672552+Xmichaela23@users.noreply.github.com>
2025-11-06 09:42:19 -0600

    Clean up temporary SQL file

--- check_computed_pace.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-  id,
-  distance,
-  moving_time,
-  duration,
-  computed->'overall'->>'avg_pace_s_per_mi' as computed_pace,
-  computed->'overall' as overall_computed
-FROM workouts 
-WHERE id = '50e9efa9-9505-4d53-b1bf-bc0bf534236f';
