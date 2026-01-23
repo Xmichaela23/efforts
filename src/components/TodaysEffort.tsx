f9280699c3280c1e9e54ad91bcb2b61b026ed33d
Xmichael23 <217672552+Xmichaela23@users.noreply.github.com>
2026-01-22 19:33:50 -0800

    Fix syntax error - remove premature closing div, keep Drawer inside main container

--- src/components/TodaysEffort.tsx
+++ src/components/TodaysEffort.tsx
@@ -1401,7 +1401,6 @@ const TodaysEffort: React.FC<TodaysEffortProps> = ({
         )}
         </div>
         </div>
-      </div>
 
       {/* Planned Workout Bottom Sheet */}
       <Drawer open={!!selectedPlannedWorkout} onOpenChange={(open) => !open && setSelectedPlannedWorkout(null)}>
